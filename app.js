02b17d5c5dfee892aea59f7b1726a1d86446d6bc
Lucas8901 <lucas25011989@gmail.com>
2023-01-23 18:49:45 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,8 @@
+function imgSlider(trocar){
+  document.querySelector('.start').src = trocar;
+}
+
+function changeCircleColor(color){
+  const circle = document.querySelector('.circle');
+  circle.style.background = color;
+}
